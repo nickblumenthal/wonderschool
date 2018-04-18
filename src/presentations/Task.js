458759680a3fceb2b2f3c4f17e839ef2749231c3
Nick Blumenthal <nrblum01@gmail.com>
2018-04-17 23:22:47 -0700

    Add individual task view

--- src/presentations/Task.js
+++ src/presentations/Task.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
+import Completed from '../assets/Completed.svg'
+import Incomplete from '../assets/Incomplete.svg'
+import Locked from '../assets/Locked.svg'
 
 class Task extends Component {
   constructor(props) {
     super(props)
   }
 
+  imageSrc() {
+    if(this.props.isLocked) {
+      return Locked
+    } else if(!this.props.completedAt) {
+      return Incomplete 
+    } else {
+      return Completed
+    }
+  }
+
+
   render() {
-    var { name, completedAt, isLocked }
+    var { name, ...props } = this.props;
     return (
-        <div></div>
+        <div {...props}>
+          <img src={this.imageSrc()} /> {name}
+        </div>
     )
   }
 }
+
+export default Task;
