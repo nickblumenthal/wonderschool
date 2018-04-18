daa35be231df846c0f209ad118e659eec7da3517
Nick Blumenthal <nrblum01@gmail.com>
2018-04-17 21:17:45 -0700

    Add logic to click groups in order to select specific task group

--- src/presentations/TaskGroups.js
+++ src/presentations/TaskGroups.js
@@ -3,15 +3,35 @@ import React, { Component } from 'react';
 class TaskGroups extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      selectedGroup: null
+    }
+  }
+
+  selectGroup(name) {
+    this.setState({
+      selectedGroup: name
+    })
   }
 
   renderTaskGroups() {
     let groupNames = Object.keys(this.props.taskGroups);
     return groupNames.map((name) => {
+      let taskCount = this.props.taskGroups[name].length;
       return (
-          <div>{name}</div>
+          <div key={name} onClick={() => {this.selectGroup(name)}}>
+            <h1>{name}</h1>
+            <span>X of {taskCount} tasks complete</span>
+          </div>
       )
     })
+  }
+
+  renderSelectedGroup() {
+    if(!this.state.selectedGroup) { return; }
+    return(
+        <div>{this.state.selectedGroup}</div>
+    )
   }
 
   render() {
@@ -19,6 +39,7 @@ class TaskGroups extends Component {
       <div>
         <h1>Things To Do</h1>
         {this.renderTaskGroups()}
+        {this.renderSelectedGroup()}
       </div>
     );
   }
