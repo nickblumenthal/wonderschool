9155cf364f24fec7816f8fc88f90ebcbfd6e3151
Nick Blumenthal <nrblum01@gmail.com>
2018-04-18 21:51:43 -0700

    Add styling and prop types for task grouping

--- src/presentations/TaskGroup.js
+++ src/presentations/TaskGroup.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
+import PropTypes from 'prop-types';
 
 import TaskList from './TaskList';
 import Group from '../assets/Group.svg'
@@ -8,11 +9,11 @@ class TaskGroup extends Component {
   render() {
     var { name, completedTaskCount, totalTaskCount, ...props } = this.props;
     return (
-          <Row key={name} {...props} className="border-bottom">
+          <Row key={name} {...props} className="border-bottom task-row no-gutters">
             <Col xs={1} className="d-flex align-items-center">
               <img src={Group} />
             </Col>
-            <Col xs={11} className="text-left">
+            <Col xs={11} className="text-left pl-1">
               <h6 className="mt-3 mb-0">{name}</h6>
               <div className="mb-3 mt-0 task-count">{completedTaskCount} of {totalTaskCount} tasks complete</div>
             </Col>
@@ -21,5 +22,11 @@ class TaskGroup extends Component {
         )
   }
 }
+
+TaskGroup.propTypes = {
+  completedTaskCount: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  totalTaskCount: PropTypes.number.isRequired
+};
 
 export default TaskGroup;
