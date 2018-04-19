d397adeb2e27a218d76a9b5d5bde3fe5ba233293
Nick Blumenthal <nrblum01@gmail.com>
2018-04-18 21:51:26 -0700

    Individual task view

--- src/presentations/Task.js
+++ src/presentations/Task.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import Completed from '../assets/Completed.svg'
 import Incomplete from '../assets/Incomplete.svg'
 import Locked from '../assets/Locked.svg'
-import { Container, Row, Col } from 'reactstrap';
+import { Row, Col } from 'reactstrap';
+import PropTypes from 'prop-types';
 
 class Task extends Component {
   constructor(props) {
@@ -18,16 +19,37 @@ class Task extends Component {
       return Completed
     }
   }
-
-
+  
+  assignClasses() {
+    if(this.props.isLocked) {
+      return 'locked'
+    } else if(!this.props.completedAt) {
+      return 'incomplete'
+    } else {
+      return 'completed'
+    }
+  }
+  
   render() {
-    var { name, ...props } = this.props;
+    var { name, onClick } = this.props;
     return (
-        <Row {...props}>
-          <img src={this.imageSrc()} /> {name}
+        <Row onClick={onClick} className="border-bottom task-row no-gutters task">
+          <Col xs={2} className="d-flex align-items-center">
+            <img src={this.imageSrc()} />
+          </Col>
+          <Col xs={10} className="d-flex text-left align-items-center">
+            <span className={this.assignClasses()}>{name}</span>
+          </Col>
         </Row>
     )
   }
 }
+
+Task.propTypes = {
+  completedAt: PropTypes.number,
+  isLocked: PropTypes.bool,
+  name: PropTypes.string.isRequired,
+  onClick: PropTypes.func
+};
 
 export default Task;
